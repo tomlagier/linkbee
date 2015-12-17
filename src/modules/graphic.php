109170d33dea7e4921c4a21fe6cc3757d511fdc8
Tom Lagier <tom@rosetta.com>
2015-12-16 23:27:07 -0800

    Interactive graphic, more

--- src/modules/graphic.php
+++ src/modules/graphic.php
@@ -3,18 +3,23 @@
     <img class="lightbulb" src="<?php echo linkbee_uri() ?>assets/images/graphic/lightbulb.png"/>
     <div class="icon-wrapper lifestyle">
       <?php require(linkbee_path() . 'modules/icons/lifestyle.php') ?>
+      <div class="icon-text">Tech Lifestyle</div>
     </div>
     <div class="icon-wrapper security">
       <?php require(linkbee_path() . 'modules/icons/security.php') ?>
+      <div class="icon-text">Security</div>
     </div>
     <div class="icon-wrapper living">
       <?php require(linkbee_path() . 'modules/icons/living.php') ?>
+      <div class="icon-text">Independent</div>
     </div>
     <div class="icon-wrapper energy">
       <?php require(linkbee_path() . 'modules/icons/energy.php') ?>
+      <div class="icon-text">Energy</div>
     </div>
     <div class="icon-wrapper heart">
       <?php require(linkbee_path() . 'modules/icons/heart.php') ?>
+      <div class="icon-text">Health</div>
     </div>
   </div>
 </div>
